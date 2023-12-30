7f110a64db5918da7b705a139c5f70c1164993e3
Katsu <jordanjjana@gmail.com>
2023-12-30 14:27:38 +0000

    added some stuff (forgot)

--- test/src/App.jsx
+++ test/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <>
-      <h1 className='name1'>Yo it&#39;s k5</h1>
+      <h1 className='name1'>Yo it&#39;s k5!</h1>
       <h1 className='name2'>Fullstack dev</h1>
     </>
   );
